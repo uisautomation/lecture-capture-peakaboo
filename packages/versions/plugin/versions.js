9ddba480c5f4385fc8c96badb8c7bf2874e46936
Paul Pettit <p.pettit@keele.ac.uk>
2017-03-31 17:30:42 +0100

    make versions stuff work (crudely!) when building using gitlab ci

--- packages/versions/plugin/versions.js
+++ packages/versions/plugin/versions.js
@@ -8,13 +8,20 @@ class VersionCompiler {
         let packageData = JSON.parse(file.getContentsAsString());
         versions.version = packageData.version;
 
-        let head = Plugin.fs.readFileSync('.git/HEAD', {encoding: 'utf8'});
-        let ref = head.match(/ref\:\s+(refs\/.+?\/(.+))\s/);
-        let refPath = '.git/' + ref[1];
-        versions.branch = ref[2];
+        try {
+          let head = Plugin.fs.readFileSync('.git/HEAD', {encoding: 'utf8'});
+          let ref = head.match(/ref\:\s+(refs\/.+?\/(.+))\s/);
+          let refPath = '.git/' + ref[1];
+          versions.branch = ref[2];
 
-        let commit = Plugin.fs.readFileSync(refPath, {encoding: 'utf8'});
-        versions.commit = commit.trim();
+          let commit = Plugin.fs.readFileSync(refPath, {encoding: 'utf8'});
+          versions.commit = commit.trim();
+        } catch (e) {
+          let ciVersions = Plugin.fs.readFileSync('VERSIONS', {encoding: 'utf8'});
+          ciVersions = ciVersions.split('\n');
+          versions.branch = ciVersions[0];
+          versions.commit = ciVersions[1];
+        }
 
         file.addAsset({
           path: 'versions.json',
